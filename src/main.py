18360cf5e9c21236beba4600271668c740410a63
Mila Klimovich <mila.k.klimovich@gmail.com>
2025-04-27 00:11:39 +0300

    some files edited

--- src/main.py
+++ src/main.py
@@ -1,16 +1,17 @@
-# from api_connection import get_pokemon_data
-from api_connection import get_all_pokemon_data
-
-# pikachu = get_pokemon_data("pikachu")
-# print(pikachu)
-
-def main():
+from api_connection import get_all_pokemon_data  
+def display_pokemon_data():
     data = get_all_pokemon_data()
     if data:
         for pokemon in data:
             print(f"ID: {pokemon['id']}, Name: {pokemon['name']}, Types: {pokemon['types']}")
     else:
         print("No data")
+
+
+def main():
+    print("Displaying Pokémon Data:")
+    display_pokemon_data()
+
 
 if __name__ == "__main__":
     main()
